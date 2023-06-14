be53837363222ded2698f74ca35655ef87640c98
Fernando Aponte <fernandoaponte90@gmail.com>
2023-06-13 23:44:36 -0400

    feat: added dummy data

---
+++ apiOne/src/assets/dummy.js
@@ -0,0 +1,7 @@
+let users = [
+    { id: 1, name: 'John', email: 'john@example.com' },
+    { id: 2, name: 'Sarah', email: 'sarah@example.com' },
+    { id: 3, name: 'Mark', email: 'mark@example.com' }
+];
+
+module.exports = users;
