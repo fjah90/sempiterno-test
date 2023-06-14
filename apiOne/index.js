407643bd14365b2ac28d7d711ca3423264662ce1
Fernando Aponte <fernandoaponte90@gmail.com>
2023-06-13 23:45:29 -0400

    feat: update index with routes and run app

--- apiOne/index.js
+++ apiOne/index.js
@@ -1,3 +1,13 @@
-const express = require('express')
+const express = require('express');
 const dotenv = require('dotenv').config()
-const app = express()
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+
+const usersRoutes = require('./src/routes/usersRoutes');
+app.use('/users', usersRoutes);
+
+app.listen(port, () => {
+    console.log(`App listening at http://localhost:${port}`);
+});
