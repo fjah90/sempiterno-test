087fc77db53d47c75c433854101649255208e4c2
Fernando Aponte <fernandoaponte90@gmail.com>
2023-06-14 08:16:38 -0400

    fix: some minor fix

--- apiThree/src/controllers/authController.js
+++ apiThree/src/controllers/authController.js
@@ -10,21 +10,19 @@ authController.signup = async (req, res) => {
     const { name, email, password } = req.body;
 
     try {
-        // Verificar si el usuario ya existe
         const existingUser = await User.findByEmail(email);
         if (existingUser) {
-            return res.status(400).json({ message: 'El correo electrónico ya está en uso' });
+            return res.status(400).json({ error: 'Email is already in use' });
         }
 
-        // Crear un nuevo usuario
         const user = new User({ name, email, password });
         await user.save();
-        // Generar un token JWT y devolverlo al cliente
+
         const token = jwt.sign({ userId: user.id }, JWT_SECRET);
-        res.json({ token });
+       res.status(200).json({ message: 'Sucess', token });
     } catch (error) {
-        console.error(error);
-        res.status(500).json({ message: 'Error al crear el usuario' });
+
+        res.status(500).json({ error: error.message });
     }
 };
 
@@ -32,23 +30,20 @@ authController.login = async (req, res) => {
     const { email, password } = req.body;
 
     try {
-        // Buscar al usuario por su email y verificar su contraseña
         const user = await User.findByEmail(email);
         if (!user) {
-            return res.status(401).json({ message: 'Credenciales inválidas' });
+            return res.status(401).json({ message: 'Invalid credentials' });
         }
 
         const isPasswordValid = await user.comparePassword(password);
         if (!isPasswordValid) {
-            return res.status(401).json({ message: 'Credenciales inválidas' });
+            return res.status(401).json({ error: 'Invalid credentials' });
         }
 
-        // Generar un token JWT y devolverlo al cliente
         const token = jwt.sign({ userId: user.id }, JWT_SECRET);
-        res.json({ token });
+       res.status(200).json({ message: 'Sucess', token });
     } catch (error) {
-        console.error(error);
-        res.status(500).json({ message: 'Error al iniciar sesión' });
+        res.status(500).json({ error: error.message });
     }
 };
 
--- apiThree/src/controllers/userController.js
+++ apiThree/src/controllers/userController.js
@@ -1,40 +1,38 @@
 const User = require('../services/userServices');
 
-exports.getAllUsers = async (req, res, next) => {
+exports.getAllUsers = async (req, res) => {
     try {
         const users = await User.findAll();
-        res.status(200).json({ users });
+        res.status(200).json({ message: 'Sucess', data: users});
     } catch (error) {
-        next(error);
+        res.status(404).send({ error: error.message });
     }
 };
 
-exports.getUserById = async (req, res, next) => {
+exports.getUserById = async (req, res) => {
     try {
         const user = await User.findById(req.params.id);
-        res.status(200).json({ user });
+        res.status(200).json({ message: 'Sucess', data: user });
     } catch (error) {
-        res.status(404).send('User not found');
+        res.status(404).send({ error: error.message });
     }
 };
 
-exports.createUser = async (req, res, next) => {
+exports.createUser = async (req, res) => {
     try {
         const { name, email, password } = req.body;
         const user = new User({ name, email, password });
         await user.save();
-        res.status(201).json({ user });
+        res.status(201).json({ message: 'User created sucess', data: user });
     } catch (error) {
-        next(error);
+        res.status(404).send({ error: error.message });
     }
 };
 
-exports.updateUser = async (req, res, next) => {
+exports.updateUser = async (req, res) => {
     try {
         const { id, name, email, password } = req.body;
         const user = await User.findById(req.params.id);
-
-        // console.log(user)
 
         user.id = id || user.id;
         user.name = name || user.name;
@@ -44,22 +42,22 @@ exports.updateUser = async (req, res, next) => {
         const update = await user.update(user);
 
         if (update.check){
-            res.status(200).json({ message: 'User updated sucess', data: user });
+            res.status(201).json({ message: 'User updated sucess', data: update.user });
         }else{
-            return res.status(404).json({ message: 'El correo electrónico ya está en uso' });
+            return res.status(404).json({ message: 'Email is already in use' });
         }
     } catch (error) {
-        res.status(404).send('User not found');
+        res.status(404).send({ error: error.message });
     }
 };
 
-exports.deleteUser = async (req, res, next) => {
+exports.deleteUser = async (req, res) => {
     try {
         const user = await User.findById(req.params.id);
 
         await user.delete();
-        res.status(200).send('User delete sucess');
+        res.status(200).send({ message: `User ${id} delete sucess` });
     } catch (error) {
-        res.status(404).send('User not found');
+        res.status(404).send({ error: error.message });
     }
 };
