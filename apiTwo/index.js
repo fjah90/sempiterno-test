1a2c2a1b6998f9efb64390dfe0123d766c6915cb
Fernando Aponte <fernandoaponte90@gmail.com>
2023-06-14 01:31:04 -0400

    feat: implement swagger ui

--- apiTwo/index.js
+++ apiTwo/index.js
@@ -6,6 +6,10 @@ const port = process.env.PORT || 3000;
 app.use(express.json());
 app.use(express.static('public'));
 
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
+app.use('/api', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
 const imageRoutes = require('./src/routes/imageRoutes');
 app.use('/', imageRoutes);
 
