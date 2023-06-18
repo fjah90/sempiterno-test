2f78d13b391fe789b26f3b245613548c251414cd
Fernando Aponte <fernandoaponte90@gmail.com>
2023-06-18 00:43:48 -0400

    feat: imports all application modules

--- nest-api/src/modules/application/application.module.ts
+++ nest-api/src/modules/application/application.module.ts
@@ -1,11 +1,17 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
 import { DocumentsModule } from './documents/documents.module';
+import { ImageDownloadModule } from './image-downloads/image-downloads.module';
+import { AuthModule } from './auth/auth.module';
+import { MessageModule } from './rabbitmq/message.module';
 
 @Module({
     imports: [
+        AuthModule,
         UsersModule,
-        DocumentsModule
+        DocumentsModule,
+        ImageDownloadModule,
+        MessageModule
     ]
 })
 export class ApplicationModule { }
